73f9e5caa6da4719c913959e07f17f29e297162a
Torben Jakobsen <TorbenJakobsen@users.noreply.github.com>
2025-05-27 10:33:12 +0200

    add to placeholder

--- matrix_rain_trails.py
+++ matrix_rain_trails.py
@@ -7,6 +7,16 @@ from random_list import RandomList
 class MatrixTrails:
 
     def __init__(self: Self, width: int) -> None:
+
+        # argument validation
+        if type(width) is not int:
+            raise ValueError("argument is not integer")
+        if width < 1:
+            raise ValueError(f"argument is {width}; expected >= 1")
+
         self._active: list[MatrixRainTrail] = []
         self._exhausted: list[MatrixRainTrail] = []
         self._available = RandomList(width)
+
+    def available_column_numbers(self: Self) -> RandomList:
+        return self._available
